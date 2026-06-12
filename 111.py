d2e34239a7217090742191af220073e7e8034253
GSB-ART <3144367457@qq.com>
2026-06-12 21:09:19 +0800

    third commit

--- 111.py
+++ 111.py
@@ -1,10 +1,10 @@
-import numpy as np 
-from matplotlib import pyplot as plt 
- 
-x = np.arange(1,11) 
-y =  2  * x +  5 
-plt.title("Matplotlib demo") 
-plt.xlabel("x axis caption") 
-plt.ylabel("y axis caption") 
-plt.plot(x,y) 
-plt.show()
+# 一个参数 默认起点0，步长为1 输出：[0 1 2 3 4 5]
+import numpy as np
+a = np.arange(5)
+print(a)
+ # 两个参数 默认步长为1 输出[3 4 5]
+a = np.arange(3,6)
+print(a)
+# 三个参数 起点为0，终点为3，步长为0.5 输出[ 0.   0.5   1.   1.5   2.   2.5]
+a = np.arange(0, 3, 0.5)
+print(a)
