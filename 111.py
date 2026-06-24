c68d95804b13a8b4dc6d6a78301834bb2a3c3341
GSB-ART <3144367457@qq.com>
2026-06-24 18:08:11 +0800

    fourth commit

--- 111.py
+++ 111.py
@@ -1,10 +1,2 @@
-# 一个参数 默认起点0，步长为1 输出：[0 1 2 3 4 5]
 import numpy as np
-a = np.arange(5)
-print(a)
- # 两个参数 默认步长为1 输出[3 4 5]
-a = np.arange(3,6)
-print(a)
-# 三个参数 起点为0，终点为3，步长为0.5 输出[ 0.   0.5   1.   1.5   2.   2.5]
-a = np.arange(0, 3, 0.5)
-print(a)
+print(np.__version__)
