acd6771f3f3389c2354c950fc46ff8c46afd3726
GSB-ART <3144367457@qq.com>
2026-06-29 16:09:56 +0800

    修改

--- 111.py
+++ 111.py
@@ -1,11 +1,30 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-# 假设 X 是特征矩阵，y 是目标变量
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# 自己创建数据
+data = {
+    "面积": [80, 90, 100, 110, 120],
+    "价格": [180, 200, 220, 250, 270]
+}
+
+df = pd.DataFrame(data)
+
+X = df[["面积"]]
+y = df["价格"]
+
+# 划分训练集和测试集
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 
 model = LinearRegression()
+
 model.fit(X_train, y_train)
 
-# 预测
 y_pred = model.predict(X_test)
+
+print(y_pred)
