331790215988288a8f2fdc6a4f38e733c4731777
GSB-ART <3144367457@qq.com>
2026-06-29 12:57:52 +0800

    修改

--- 111.py
+++ 111.py
@@ -1,2 +1,11 @@
-import numpy as npp
-print(np.__version__)
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# 假设 X 是特征矩阵，y 是目标变量
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# 预测
+y_pred = model.predict(X_test)
